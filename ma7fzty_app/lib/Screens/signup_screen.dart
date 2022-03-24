74ae52168d22cb76affb448aa7bf1129c0819f02
kareem644 <kareem1802405@miuegypt.edu.eg>
2022-03-24 16:51:55 +0200

    update

--- ma7fzty_app/lib/Screens/signup_screen.dart
+++ ma7fzty_app/lib/Screens/signup_screen.dart
@@ -25,224 +25,211 @@ class _SignUp extends State<SignUp> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // drawer: IconButton(
-      //                 onPressed: () async {
+        // drawer: IconButton(
+        //                 onPressed: () async {
 
-      //                   Navigator.pushNamed(context, '/login');
-      //                 }, icon: Icon(Icons.arrow_back),),
-        backgroundColor: const Color.fromARGB(255, 209, 207, 207),
+        //                   Navigator.pushNamed(context, '/login');
+        //                 }, icon: Icon(Icons.arrow_back),),
+        backgroundColor: const Color.fromARGB(255, 255, 255, 255),
         appBar: AppBar(
-          
           title: Row(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               Image.asset(
-                    'images/logo.png',
-                    fit: BoxFit.contain,
-                    height: 50,
-                    width: 50,
-                  ),
+                'images/logo.png',
+                fit: BoxFit.contain,
+                height: 50,
+                width: 50,
+              ),
               const Text(
                 ' SignUp',
                 style: TextStyle(color: Colors.white),
               ),
-              
             ],
-            
           ),
-           centerTitle: true,
-              // automaticallyImplyLeading: false,
-              backgroundColor: const Color.fromARGB(255, 71, 196, 79),
+          centerTitle: true,
+          // automaticallyImplyLeading: false,
+          backgroundColor: const Color.fromARGB(255, 71, 196, 79),
         ),
-
-        body: 
-            ListView(
-              children: [
-              Container(
-                padding: const EdgeInsets.only(left: 40, top: 25, right: 40),
-                //child: GestureDetector(
-                  //onTap: () {
-                    //FocusScope.of(context).unfocus();
-                  //},
-                  child: Column(children: [
-              const SizedBox(height: 50),
-              const Center(
-                  child: Text(
-                'Sign Up',
-                style: TextStyle(
-                    color: Colors.black,
-                    fontWeight: FontWeight.bold,
-                    fontStyle: FontStyle.italic,
-                    fontSize: 30),
-              )),
-              const SizedBox(height: 50),
-              TextFormField(
-                textAlign: TextAlign.center,
-                style: const TextStyle(
-                          fontSize: 18,
-                          color: Colors.white,
+        body: ListView(
+          children: [
+            Container(
+              padding: const EdgeInsets.only(left: 40, top: 25, right: 40),
+              //child: GestureDetector(
+              //onTap: () {
+              //FocusScope.of(context).unfocus();
+              //},
+              child: Column(
+                children: [
+                  const SizedBox(height: 50),
+                  const Center(
+                      child: Text(
+                    'Sign Up',
+                    style: TextStyle(
+                        color: Colors.black,
+                        fontWeight: FontWeight.bold,
+                        fontStyle: FontStyle.italic,
+                        fontSize: 30),
+                  )),
+                  const SizedBox(height: 50),
+                  TextFormField(
+                    textAlign: TextAlign.center,
+                    style: const TextStyle(
+                      fontSize: 18,
+                      color: Colors.white,
+                    ),
+                    keyboardType: TextInputType.emailAddress,
+                    controller: e,
+                    decoration: const InputDecoration(
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(15.0)),
+                        borderSide: BorderSide.none,
+                      ),
+                      filled: true,
+                      fillColor: Color.fromARGB(255, 71, 196, 79),
+                      hintText: 'Email',
+                      hintStyle: TextStyle(fontSize: 18, color: Colors.white),
+                    ),
+                  ),
+                  const SizedBox(height: 25),
+                  TextFormField(
+                    textAlign: TextAlign.center,
+                    style: const TextStyle(
+                      fontSize: 18,
+                      color: Colors.white,
+                    ),
+                    keyboardType: TextInputType.emailAddress,
+                    controller: pN,
+                    maxLength: 11,
+                    inputFormatters: <TextInputFormatter>[
+                      FilteringTextInputFormatter.deny(RegExp('[^0-9]')),
+                    ],
+                    decoration: const InputDecoration(
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(15.0)),
+                        borderSide: BorderSide.none,
+                      ),
+                      filled: true,
+                      fillColor: Color.fromARGB(255, 71, 196, 79),
+                      hintText: 'Phone Number',
+                      hintStyle: TextStyle(fontSize: 18, color: Colors.white),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 25.0,
+                  ),
+                  TextFormField(
+                    textAlign: TextAlign.center,
+                    style: const TextStyle(fontSize: 18, color: Colors.white),
+                    //keyboardType: TextInputType.visiblePassword,
+                    controller: p,
+                    decoration: const InputDecoration(
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(15.0)),
+                        borderSide: BorderSide.none,
+                      ),
+                      filled: true,
+                      fillColor: Color.fromARGB(255, 71, 196, 79),
+                      hintText: 'Password',
+                      hintStyle: TextStyle(fontSize: 18, color: Colors.white),
+                    ),
+                    obscureText: true,
+                  ),
+                  const SizedBox(
+                    height: 25.0,
+                  ),
+                  TextFormField(
+                    textAlign: TextAlign.center,
+                    style: const TextStyle(fontSize: 18, color: Colors.white),
+                    controller: cP,
+                    decoration: const InputDecoration(
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(15.0)),
+                        borderSide: BorderSide.none,
+                      ),
+                      filled: true,
+                      fillColor: Color.fromARGB(255, 71, 196, 79),
+                      hintText: 'Confirm Password',
+                      hintStyle: TextStyle(fontSize: 18, color: Colors.white),
+                    ),
+                    obscureText: true,
+                  ),
+                  const SizedBox(
+                    height: 25.0,
+                  ),
+                  SizedBox(
+                    height: 50,
+                    child: Padding(
+                      padding: const EdgeInsets.only(left: 80, right: 80),
+                      child: ElevatedButton(
+                        child: const Text(
+                          'Sign Up',
                         ),
-                keyboardType: TextInputType.emailAddress,
-                controller: e,
-              decoration: InputDecoration(
-                          border: OutlineInputBorder(
-                            borderRadius:
-                                BorderRadius.all(Radius.circular(15.0)),
-                            borderSide: BorderSide.none,
+                        style: ButtonStyle(
+                          backgroundColor: MaterialStateProperty.all(
+                            const Color.fromARGB(255, 71, 196, 79),
                           ),
-                          filled: true,
-                          fillColor: Color.fromARGB(255, 71, 196, 79),
-                          hintText: 'Email',
-                          hintStyle:
-                              TextStyle(fontSize: 18, color: Colors.white),
-                        ),    
-              ),
-
-              const SizedBox(height: 25),
-              TextFormField(
-                textAlign: TextAlign.center,
-                style: const TextStyle(
-                  fontSize: 18,
-                  color: Colors.white,
-                ),
-                keyboardType: TextInputType.emailAddress,
-                controller: pN,
-                maxLength: 11,
-                inputFormatters: <TextInputFormatter>[
-                  FilteringTextInputFormatter.deny(RegExp('[^0-9]')),
-                ],
-               
-                 decoration: InputDecoration(
-                          border: OutlineInputBorder(
-                            borderRadius:
-                                BorderRadius.all(Radius.circular(15.0)),
-                            borderSide: BorderSide.none,
-                          ),
-                          filled: true,
-                          fillColor: Color.fromARGB(255, 71, 196, 79),
-                          hintText: 'Phone Number',
-                          hintStyle:
-                              TextStyle(fontSize: 18, color: Colors.white),
-                        ), 
-
-              ),
-              const SizedBox(
-                height: 25.0,
-              ),
-              TextFormField(
-                textAlign: TextAlign.center,
-                style: const TextStyle(fontSize: 18, color: Colors.white),
-                //keyboardType: TextInputType.visiblePassword,
-                controller: p,
-                decoration: InputDecoration(
-                  border: const OutlineInputBorder(
-                    borderRadius: BorderRadius.all(Radius.circular(15.0)),
-                    borderSide: BorderSide.none,
-                  ),
-                  filled: true,
-                  fillColor:Color.fromARGB(255, 71, 196, 79),
-                  hintText: 'Password',
-                  hintStyle: const TextStyle(fontSize: 18, color: Colors.white),
-                ),
-                obscureText: true,
-              ),
-              const SizedBox(
-                height: 25.0,
-              ),
-              TextFormField(
-                textAlign: TextAlign.center,
-                style: const TextStyle(fontSize: 18, color: Colors.white),
-                controller: cP,
-                decoration: InputDecoration(
-                  border: const OutlineInputBorder(
-                    borderRadius: BorderRadius.all(Radius.circular(15.0)),
-                    borderSide: BorderSide.none,
-                  ),
-                  filled: true,
-                  fillColor: Color.fromARGB(255, 71, 196, 79),
-                  hintText: 'Confirm Password',
-                  hintStyle: const TextStyle(fontSize: 18, color: Colors.white),
-                ),
-                obscureText: true,
-              ),
-              const SizedBox(
-                height: 25.0,
-              ),
-              SizedBox(
-                height: 50,
-                child: Padding(
-                  padding: const EdgeInsets.only(left: 80, right: 80),
-                  child: ElevatedButton(
-                    child: const Text(
-                      'Sign Up',
-                    ),
-                    style: ButtonStyle(
-                      backgroundColor:
-                          MaterialStateProperty.all(Color.fromARGB(255, 71, 196, 79),),
-                    ),
-                    onPressed: () async => {
-                      if (pN.text == '' ||
-                          e.text == '' ||
-                          p.text == '' ||
-                          cP.text == '')
-                        {
-                          text = 'Error, Please fill all requirements',
-                          showAlertDialog(context),
-                        }
-                      else if (p.text != cP.text)
-                        {
-                          text = 'Error, Password does not match',
-                          showAlertDialog(context),
-                        }
-                      else if (!e.text.contains('@'))
-                        {
-                          text = 'Email format is not applicable',
-                          showAlertDialog(context),
-                        }
-                      else if (p.text.length <= 6)
-                        {
-                          text = ' Weak Password !',
-                          showAlertDialog(context),
-                        }
-                      else if (pN.text.length != 11)
-                        {
-                          text = ' Phone Number is less than 11 Numbers !',
-                          showAlertDialog(context),
-                        }
-                      else
-                        {
-                          if (await user.signUp(
-                                  phoneNumber: pN.text,
-                                  email: e.text,
-                                  password: p.text) ==
-                              'Error')
+                        ),
+                        onPressed: () async => {
+                          if (pN.text == '' ||
+                              e.text == '' ||
+                              p.text == '' ||
+                              cP.text == '')
                             {
-                              text =
-                                  'Error Email is Aleady Exist OR There are an error in Data',
+                              text = 'Error, Please fill all requirements',
+                              showAlertDialog(context),
+                            }
+                          else if (p.text != cP.text)
+                            {
+                              text = 'Error, Password does not match',
+                              showAlertDialog(context),
+                            }
+                          else if (!e.text.contains('@'))
+                            {
+                              text = 'Email format is not applicable',
+                              showAlertDialog(context),
+                            }
+                          else if (p.text.length <= 6)
+                            {
+                              text = ' Weak Password !',
+                              showAlertDialog(context),
+                            }
+                          else if (pN.text.length != 11)
+                            {
+                              text = ' Phone Number is less than 11 Numbers !',
                               showAlertDialog(context),
                             }
                           else
                             {
-                              Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                    builder: (context) => logIn()),
-                              )
+                              if (await user.signUp(
+                                      phoneNumber: pN.text,
+                                      email: e.text,
+                                      password: p.text) ==
+                                  'Error')
+                                {
+                                  text =
+                                      'Error Email is Aleady Exist OR There are an error in Data',
+                                  showAlertDialog(context),
+                                }
+                              else
+                                {
+                                  Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                        builder: (context) => logIn()),
+                                  )
+                                }
                             }
-                        }
-                    },
+                        },
+                      ),
+                    ),
                   ),
-                ),
+                ],
               ),
-
-                  ],
-                  ),
-
-                ),
-              //)
-            ],
-            )
-            );
+            ),
+            //)
+          ],
+        ));
   }
 
   showAlertDialog(
--- ma7fzty_app/lib/main.dart
+++ ma7fzty_app/lib/main.dart
@@ -2,7 +2,6 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:ma7fzty_app/Screens/login_screen.dart';
-import 'package:ma7fzty_app/Screens/signup_screen.dart';
 import 'package:ma7fzty_app/Screens/splash_screen.dart';
 
 Future<void> main() async {
@@ -25,15 +24,11 @@ class MyApp extends StatelessWidget {
     ]);
     return MaterialApp(
       routes: {
-        
-
-        '/logIn': (context) =>logIn(),
-
-
+        '/logIn': (context) => logIn(),
       },
-     // home: logIn(),
-    home: Splash(),
-   //home:SignUp(),
+      // home: logIn(),
+      home: const Splash(),
+      //home:SignUp(),
     );
   }
 }
