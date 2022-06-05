2763aa801b55c90f64bbed41903a90a063799f0a
Ganna3 <gannaayman57@gmail.com>
2022-06-05 22:00:18 +0200

    experiments

--- seqPar.py
+++ seqPar.py
@@ -39,6 +39,8 @@ if __name__ == '__main__':
     user1.myAmount(1000)
     user2.myAmount(900)
     user3.myAmount(500)
+
+    
     
     startSequential = time.perf_counter()
     transferMoneySequential(user1, user2, 200)
@@ -46,17 +48,21 @@ if __name__ == '__main__':
     transferMoneySequential(user3, user1, 300)
     endSequential = time.perf_counter()
     sequential = (endSequential - startSequential)
+    
+    
     print('Duration Time of Running Sequential {}'.format(sequential))
     print('{} Amount After Transfer: {}'.format(user1.getName(), user1.getAmount()))
     print('{} Amount After Transfer: {}'.format(user2.getName(), user2.getAmount()))
     print('{} Amount After Transfer: {}'.format(user3.getName(), user3.getAmount()))
+    
+    
     #PARALLELE WORK
     add = threading.Lock()
     remove = threading.Lock()
     startTime = time.perf_counter()
-    threading.Thread(target=transferMoneyParallel, args=(user1, user2, 300, add, remove)).start()
+    threading.Thread(target=transferMoneyParallel, args=(user1, user2, 200, add, remove)).start()
     threading.Thread(target=transferMoneyParallel, args=(user2, user3, 100, add, remove)).start()
-    threading.Thread(target= transferMoneyParallel, args=(user3, user1, 100, add, remove)).start()
+    threading.Thread(target= transferMoneyParallel, args=(user3, user1, 300, add, remove)).start()
     endTime = time.perf_counter()
     parallel = (endTime - startTime)
     print('\nDuration Time of Running Parallel {}'.format(parallel))
@@ -66,3 +72,5 @@ if __name__ == '__main__':
     
     print('\nSpeedup: {:.2f}%'.format(100*(sequential/parallel)))
     print('Efficiency: {:.2f}%'.format(100*(sequential/parallel)/3))
+    
+    
